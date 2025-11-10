85b91b70db09efdf68f6c66fbef53c1e2ba74d91
eriinys <eriinys@duck.com>
2025-11-09 23:14:54 -0600

    setup Menu class

---
+++ src/main/java/model/Menu.java
@@ -0,0 +1,55 @@
+package model;
+//contains read-only data that UI prints (name, toppings, price)
+public class Menu {
+    //Garlic Bread
+    private static final String gb = "Classic Garlic Bread";
+    private static final double gbPrice = 4.50;
+
+    //Regular Topping
+    private static final String rt1 = "Garlic Mushroom";
+    private static final double rt1Price = 1.50;
+    private static final String rt2 = "Caremelized Garlic";
+    private static final double rt2Price = 1.00;
+    private static final String rt3 = "Roasted Garlic";
+    private static final double rt3Price = 1.00;
+    private static final String rt4 = "Pickled Garlic";
+    private static final double rt4Price = 1.00;
+
+    //Premium Topping
+    private static final String pt1 = "Roasted Black Garlic";
+    private static final double pt1Price = 2.00;
+    private static final String pt2 = "Smoked Garlic Cheese";
+    private static final double pt2Price = 2.50;
+    private static final String pt3 = "Smoked Black Garlic Caviar";
+    private static final double pt3Price = 4.25;
+    private static final String pt4 = "Truffle-Garlic Butter Lobster";
+    private static final double pt4Price = 12.99;
+
+    //Drinks
+    private static final String d1 = "Garlic Shot (immunity booster)";
+    private static final double d1Price = 4.00;
+
+    private static final String d2 = "Frozen Black Garlic Lemonade";
+    private static final double d2Price = 8.25;
+
+    private static final String d3 = "Garlic Bloody Mary";
+    private static final double d3Price = 11.50;
+
+    //Sides
+    private static final String s1 = "Garlic Soup";
+    private static final double s1Price = 6.25;
+    private static final String s2 = "Garlic Salad";
+    private static final double s2Price = 8.25;
+    private static final String s3 = "Garlic Fries";
+    private static final double s3Price = 7.25;
+
+    //Dessert
+    private static final String ds1 = "Garlic Honey Ice Cream";
+    private static final double ds1Price = 7.25;
+    private static final String ds2 = "Garlic Rice Pudding";
+    private static final double ds2Price = 7.25;
+
+    public void printMenu(){
+
+    }
+}
