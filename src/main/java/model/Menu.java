a402bfdf17e67e742c0713ccf074bc1e577adf9f
eriinys <eriinys@duck.com>
2025-11-11 16:11:50 -0600

    implemented HashMap to Menu class used for holding menu item data and printing menu

--- src/main/java/model/Menu.java
+++ src/main/java/model/Menu.java
@@ -1,96 +1,129 @@
 package model;
+import java.util.*;
 
 public class Menu {
-    //contains read-only data that UI prints (name / price)
+    //setup static HashMap in the field that belongs specifically to the Menu class, NOT part of individual instance
+    //data is shared across every Menu object that is used for validating user input when choosing item from menu option
+
+    //region field
     //Garlic Bread
-    private static final String gb = "Classic Garlic Bread";
-    private static final double gbPrice = 4.50;
+    private static final String classicGb = "Classic Garlic Bread";
+    private static final double classicGbPrice = 4.50;
 
     //Regular Topping
-    private static final String rt1 = "Garlic Mushroom";
-    private static final double rt1Price = 1.50;
-    private static final String rt2 = "Caramelized Garlic";
-    private static final double rt2Price = 1.00;
-    private static final String rt3 = "Roasted Garlic";
-    private static final double rt3Price = 1.00;
-    private static final String rt4 = "Pickled Garlic";
-    private static final double rt4Price = 1.00;
+    private static final Map<String, String> regularTopping = new HashMap<>();
+    private static final Map<String, Double> regularPrices = new HashMap<>();
 
     //Premium Topping
-    private static final String pt1 = "Roasted Black Garlic";
-    private static final double pt1Price = 2.00;
-    private static final String pt2 = "Smoked Garlic Cheese";
-    private static final double pt2Price = 2.50;
-    private static final String pt3 = "Smoked Black Garlic Caviar";
-    private static final double pt3Price = 4.25;
-    private static final String pt4 = "Truffle-Garlic Butter Lobster";
-    private static final double pt4Price = 12.99;
+    private static final Map<String, String> premiumTopping = new HashMap<>();
+    private static final Map<String, Double> premiumPrices = new HashMap<>();
 
     //Drinks
-    private static final String d1 = "Garlic Shot (immunity booster)";
-    private static final double d1Price = 4.00;
-    private static final String d2 = "Frozen Black Garlic Lemonade";
-    private static final double d2Price = 8.25;
-    private static final String d3 = "Garlic Bloody Mary";
-    private static final double d3Price = 11.50;
+    private static final Map<String, String> drinkName = new HashMap<>();
+    private static final Map<String, Double> drinkPrices = new HashMap<>();
 
     //Sides
-    private static final String s1 = "Garlic Soup";
-    private static final double s1Price = 6.25;
-    private static final String s2 = "Garlic Salad";
-    private static final double s2Price = 8.25;
-    private static final String s3 = "Garlic Fries";
-    private static final double s3Price = 7.25;
-    private static final String s4 = "Garlic Naan";
-    private static final double s4Price = 5.75;
+    private static final Map<String, String> sideName = new HashMap<>();
+    private static final Map<String, Double> sidePrices = new HashMap<>();
 
     //Dessert
-    private static final String ds1 = "Garlic Honey Ice Cream";
-    private static final double ds1Price = 7.25;
-    private static final String ds2 = "Garlic Rice Pudding";
-    private static final double ds2Price = 7.25;
+    private static final Map<String, String> dessertName = new HashMap<>();
+    private static final Map<String, Double> dessertPrices = new HashMap<>();
+    //endregion
 
-    public void printRegularTopping(){
-        System.out.printf("""
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                """, rt1, rt1Price, rt2, rt2Price, rt3, rt3Price, rt4, rt4Price);
+    //region key/value
+    static { //used to initialize static field (HashMap) assigning key/value
+
+        //Regular Topping
+        regularTopping.put("RT1", "Garlic Mushroom");
+        regularPrices.put("RT1", 1.50);
+        regularTopping.put("RT2", "Caramelized Garlic");
+        regularPrices.put("RT2", 1.00);
+        regularTopping.put("RT3", "Roasted Garlic");
+        regularPrices.put("RT3", 1.00);
+        regularTopping.put("RT4", "Pickled Garlic");
+        regularPrices.put("RT4", 1.00);
+
+        //Premium Topping
+        premiumTopping.put("PT1", "Roasted Black Garlic");
+        premiumPrices.put("PT1", 2.00);
+        premiumTopping.put("PT2", "Smoked Garlic Cheese");
+        premiumPrices.put("PT2", 2.50);
+        premiumTopping.put("PT3", "Smoked Black Garlic Caviar");
+        premiumPrices.put("PT3", 4.25);
+        premiumTopping.put("PT4", "Truffle-Garlic Butter Lobster");
+        premiumPrices.put("PT4", 12.99);
+
+        //Drinks
+        drinkName.put("DR1", "Garlic Shot (immunity booster)");
+        drinkPrices.put("DR1", 4.00);
+        drinkName.put("DR2", "Frozen Black Garlic Lemonade");
+        drinkPrices.put("DR2", 8.25);
+        drinkName.put("DR3", "Garlic Bloody Mary");
+        drinkPrices.put("DR3", 11.50);
+
+        //Sides
+        sideName.put("SE1", "Garlic Soup");
+        sidePrices.put("SE1", 6.25);
+        sideName.put("SE2", "Garlic Salad");
+        sidePrices.put("SE2", 8.25);
+        sideName.put("SE3", "Garlic Fries");
+        sidePrices.put("SE3", 7.25);
+        sideName.put("SE4", "Garlic Naan");
+        sidePrices.put("SE4", 5.75);
+
+        //Dessert
+        dessertName.put("DS1", "Garlic Honey Ice Cream");
+        dessertPrices.put("DS1", 7.25);
+        dessertName.put("DS2", "Garlic Rice Pudding");
+        dessertPrices.put("DS2", 7.25);
+    }
+    //endregion
+
+    //region getter method
+    public static Map<String, String> getRegularTopping() {
+        return Collections.unmodifiableMap(regularTopping);
+    }
+    public static Map<String, Double> getRegularPrices() {
+        return Collections.unmodifiableMap(regularPrices);
     }
 
-    public void printPremiumTopping(){
-        System.out.printf("""
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                """, pt1, pt1Price, pt2, pt2Price, pt3, pt3Price, pt4, pt4Price);
+    public static Map<String, String> getPremiumTopping() {
+        return Collections.unmodifiableMap(premiumTopping);
+    }
+    public static Map<String, Double> getPremiumPrices() {
+        return Collections.unmodifiableMap(premiumPrices);
     }
 
-    public void printDrinks(){
-        System.out.printf("""
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                """, d1, d1Price, d2, d2Price, d3, d3Price);
+    public static Map<String, String> getDrinkName() {
+        return Collections.unmodifiableMap(drinkName);
+    }
+    public static Map<String, Double> getDrinkPrices() {
+        return Collections.unmodifiableMap(drinkPrices);
     }
 
-    public void printSides(){
-        System.out.printf("""
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                o %s      $%.2f
-                """, s1, s1Price, s2, s2Price, s3, s3Price, s4, s4Price);
+    public static Map<String, String> getSideName() {
+        return Collections.unmodifiableMap(sideName);
+    }
+    public static Map<String, Double> getSidePrices() {
+        return Collections.unmodifiableMap(sidePrices);
     }
 
-    public void printDessert(){
-        System.out.printf("""
-                o %s      $%.2f
-                o %s      $%.2f
-                """, ds1, ds1Price, ds2, ds2Price);
+    public static Map<String, String> getDessertName() {
+        return Collections.unmodifiableMap(dessertName);
     }
+    public static Map<String, Double> getDessertPrices() {
+        return Collections.unmodifiableMap(dessertPrices);
+    }
+    //endregion
 
+    //setting custom printer method for displaying each key/value (similar to toString)
+    public void printMenu(Map<String, String> names, Map<String, Double> prices){
+        for (String key : names.keySet()) { //keySet() returns collection of keys stored in HashMap
+            String value = names.get(key); //looks up the value related to the key
+            double price = prices.getOrDefault(key, 0.00); //if it can't find key, returns given default value
+            System.out.printf("[%s] %s     $%.2f%n", key, value, price);
+        }
+    }
 
 }
