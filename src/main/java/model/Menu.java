ed41604cf1a3e586804c25f2eec05ad871e86a6c
eriinys <eriinys@duck.com>
2025-11-10 18:48:33 -0600

    added print menu to call from UI

--- src/main/java/model/Menu.java
+++ src/main/java/model/Menu.java
@@ -1,6 +1,7 @@
 package model;
-//contains read-only data that UI prints (name, toppings, price)
+
 public class Menu {
+    //contains read-only data that UI prints (name / price)
     //Garlic Bread
     private static final String gb = "Classic Garlic Bread";
     private static final double gbPrice = 4.50;
@@ -8,7 +9,7 @@ public class Menu {
     //Regular Topping
     private static final String rt1 = "Garlic Mushroom";
     private static final double rt1Price = 1.50;
-    private static final String rt2 = "Caremelized Garlic";
+    private static final String rt2 = "Caramelized Garlic";
     private static final double rt2Price = 1.00;
     private static final String rt3 = "Roasted Garlic";
     private static final double rt3Price = 1.00;
@@ -28,10 +29,8 @@ public class Menu {
     //Drinks
     private static final String d1 = "Garlic Shot (immunity booster)";
     private static final double d1Price = 4.00;
-
     private static final String d2 = "Frozen Black Garlic Lemonade";
     private static final double d2Price = 8.25;
-
     private static final String d3 = "Garlic Bloody Mary";
     private static final double d3Price = 11.50;
 
@@ -42,6 +41,8 @@ public class Menu {
     private static final double s2Price = 8.25;
     private static final String s3 = "Garlic Fries";
     private static final double s3Price = 7.25;
+    private static final String s4 = "Garlic Naan";
+    private static final double s4Price = 5.75;
 
     //Dessert
     private static final String ds1 = "Garlic Honey Ice Cream";
@@ -49,7 +50,47 @@ public class Menu {
     private static final String ds2 = "Garlic Rice Pudding";
     private static final double ds2Price = 7.25;
 
-    public void printMenu(){
-
+    public void printRegularTopping(){
+        System.out.printf("""
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                """, rt1, rt1Price, rt2, rt2Price, rt3, rt3Price, rt4, rt4Price);
     }
+
+    public void printPremiumTopping(){
+        System.out.printf("""
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                """, pt1, pt1Price, pt2, pt2Price, pt3, pt3Price, pt4, pt4Price);
+    }
+
+    public void printDrinks(){
+        System.out.printf("""
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                """, d1, d1Price, d2, d2Price, d3, d3Price);
+    }
+
+    public void printSides(){
+        System.out.printf("""
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                o %s      $%.2f
+                """, s1, s1Price, s2, s2Price, s3, s3Price, s4, s4Price);
+    }
+
+    public void printDessert(){
+        System.out.printf("""
+                o %s      $%.2f
+                o %s      $%.2f
+                """, ds1, ds1Price, ds2, ds2Price);
+    }
+
+
 }
