03e849b6aeb51716e21f8ff3b71bb8b06d9cc61d
eriinys <eriinys@duck.com>
2025-11-11 21:32:03 -0600

    fixed part of receipt formatting

--- src/main/java/util/ReceiptWriter.java
+++ src/main/java/util/ReceiptWriter.java
@@ -1,5 +1,7 @@
 package util;
+
 import model.*;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,62 +20,97 @@ public class ReceiptWriter {
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
     String receiptName = datetime.format(dateTimeFormatter); //create receipt file name (by date/time)
 
-    public void saveReceipt(Order order){
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/resources/ReceiptFolder/" + receiptName))){
-
-            String line = "----------------------------------------";
-
-            bw.write(line);
-            bw.newLine();
-            bw.write("     Ultimate Garlic Shop     " +
-                    "\n     " + date.format(dateFormatter) + " at " + time.format(timeFormatter) + "     ");
-            bw.newLine();
-            bw.write(line);
-            bw.newLine();
-
-            bw.write(String.format("| %-20s | %10s |", "Item","Price"));
-            bw.newLine();
-
+    public void saveReceipt(Order order) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/resources/ReceiptFolder/" + receiptName))) {
+            //receipt format
+            int length = 0;
             for (Product product : order.getOrder()){
-                if (product.hasSize()){
-                    bw.write(String.format("| %s %s x%-20d | %10.2f |", product.getItemName(), product.getSize(), product.getQuantity() ,product.getPrice()));
+                String name = product.getItemName();
+                if (name.length() > length) {
+                    length = name.length();
+                }
+            }
+
+            length += 2;
+
+            int dashLength = length +20;
+            String dash = "-".repeat(dashLength);
+            String header = "| %-" + length + "s | %3s | %12s |%n";
+            String line = "| %-" + length + "s | %3s | $%10.2f |%n";
+
+            //receipt top
+            bw.write(dash);
+            bw.newLine();
+
+            String shopName = "Ultimate Garlic Shop";
+            bw.write(shopName);
+            bw.newLine();
+            //!!!!!!!!figure out how to do centering!!!!!!!!
+            String dateTime = date.format(dateFormatter) + " at " + time.format(timeFormatter);
+            bw.write(dateTime);
+            bw.newLine();
+
+            bw.write(dash);
+            bw.newLine();
+
+            //header
+            bw.write(String.format(header,"Item", "Qty", "Price"));
+            //no size
+            for (Product product : order.getOrder()) {
+                String name = product.getItemName();
+                int qty = product.getQuantity();
+                double unitPrice = product.getPrice();
+                double totalPrice = unitPrice * qty;
+                bw.write(String.format(line, name, qty, totalPrice));
+            }
+
+            //fix hasSize
+            String base;
+            for (Product product : order.getOrder()) {
+                if (product.hasSize()) {
+                    base = product.getItemName() + " " + product.getSize();
+                    length = Math.max(length, base.length());
+
+                    bw.write(String.format("| %s %s x%-20d | %10.2f |", product.getItemName(), product.getSize(), product.getQuantity(), product.getPrice()));
                     bw.newLine();
                 } else {
-                    bw.write(String.format("| %s    x%-20d | %10.2f |", product.getItemName(), product.getQuantity() ,product.getPrice()));
+                    bw.write(String.format("| %s    x%-20d | %10.2f |", product.getItemName(), product.getQuantity(), product.getPrice()));
                     bw.newLine();
                 }
                 //writing bread type & topping specifically for garlic bread:
-                if (product instanceof GarlicBread){
-                    bw.write(String.format("| %s %s x%-20d | %10.2f |", product.getItemName(), product.getSize(), product.getQuantity() ,product.getPrice()));
+                if (product instanceof GarlicBread) {
+                    bw.write(String.format("| %s %s x%-20d | %10.2f |", product.getItemName(), product.getSize(), product.getQuantity(), product.getPrice()));
                     bw.newLine();
                     bw.write(String.format("|  -%s %-20s | %10s |", "Bread Type:", ((GarlicBread) product).getBreadType(), ""));
                     bw.newLine();
                     GarlicBread garlicBread = (GarlicBread) product; //downcasts model.Product to model.GarlicBread inside instanceof model.GarlicBread if statement
                     List<Topping> topping = garlicBread.getToppings();
-                    if(!topping.isEmpty()){
+                    if (!topping.isEmpty()) {
                         bw.write(String.format("|  %-19s | %10s |", "-Toppings:", ""));
                         bw.newLine();
-                        for (Topping top : topping){
+                        for (Topping top : topping) {
                             bw.write(String.format("|   %s x%-18s | $%10.2f |", top.getName(), top.getPortion(), top.getTotalPrice()));
                             bw.newLine();
                         }
                     }
-                    if (garlicBread.isSpecialized()){
-                        bw.write(String.format("|  %19s | %.2f", "Special Added: Cheese Stuffed Garlic Bread"));
+                    if (garlicBread.isSpecialized()) {
+                        bw.write(String.format("|  %19s | %.2f", "Special Added: Cheese Stuffed Garlic Bread", garlicBread.getSpecialPrice()));
                         bw.newLine();
                     }
                 }
             }
-            bw.write(line);
-            bw.newLine();
-            bw.write(String.format("| %-20s | $%10.2f |", "TOTAL", order.getTotalCost()));
-            bw.newLine();
-            bw.write(line);
 
+            //total
+            bw.write(dash);
+            bw.newLine();
+            bw.write(String.format(line, "TOTAL", "", order.getTotalCost()));
+            bw.newLine();
+            bw.write(dash);
 
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
+
 }
